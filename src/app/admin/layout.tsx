a54c5a313ba686e46f88a83c87d75936a89335b2
Alexandre Houmeau <a.houmeau@shellrecharge.com>
2025-04-08 09:54:08 +0200

    Removed Navbar from admin flow

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,15 +1,13 @@
 import { SideNav } from "@/app/ui/dashboard/Sidenav";
-import { NavBar } from "../ui/dashboard/NavBar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <NavBar />
       <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div className="w-full flex-none md:w-64">
           <SideNav />
         </div>
-        <div className="flex-grow bg-slate-50 p-6 md:overflow-y-auto md:p-12">
+        <div className="flex-grow bg-white p-6 md:overflow-y-auto md:p-12">
           {children}
         </div>
       </div>
