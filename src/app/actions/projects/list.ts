5347a8d5802699a3b028d47acd9dfa22e155eb16
Alexandre Houmeau <a.houmeau@shellrecharge.com>
2025-05-13 15:22:23 +0200

    add list projects query

--- src/app/actions/projects/list.ts
+++ src/app/actions/projects/list.ts
@@ -1,8 +1,17 @@
 import { supabase } from "@/utils/supabaseClient";
 import { Project } from "./type";
+import { getPublicUrl } from "./files";
 
 export const listProjects = async (): Promise<Project[] | null> => {
   const { data } = await supabase.from("projects").select();
+
+  if (data?.length) {
+    data.map((project) => ({
+      ...project,
+      photos: getPublicUrl(project.photos),
+      blueprints: getPublicUrl(project.blueprints),
+    }));
+  }
 
   return data;
 };
